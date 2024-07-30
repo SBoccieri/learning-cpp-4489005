4f10bf631e9ec522321758d1e99107cefa40dc84
SBoccieri <seboccieri@gmail.com>
2024-07-30 19:02:07 +0000

    Completed exercises

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,9 +3,22 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
-    
+    const size_t Length1 = 25;
+
+    char array_str1[Length1] = "Hello there! ";
+    char array_str2[] = "What's up?";
+
+    std::string std_str1 = "Hello there! ";
+    std::string std_str2 = "What's up?";
+
+    strncat(array_str1, array_str2, Length1);
+    std::cout << array_str1 << std::endl;
+    std::cout << std_str1 + std_str2 << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
